1a17c1955608c08679244cb1500af9b334c7f4ae
ArasB10 <arasbraziunas10@gmail.com>
2017-03-01 21:48:39 +0200

    Update sketch.js

--- newgame/sketch.js
+++ newgame/sketch.js
@@ -2,12 +2,13 @@ var ninja;
 
 function setup() {
   createCanvas(800,400);
-  ninja = loadAnimation("assets/Dead__000.png","assets/Dead__009.png");
+  ninja = createSprite(400, 150, 20, 40); 
+  ninja.addAnimation("dead","assets/Dead__000.png","assets/Dead__009.png");
  // createSprite(400, 200, 50, 50);
 }
 
 function draw() {
   background(200);	
-  animation(ninja, 300, 100);  
-  //drawSprites();
+  //animation(ninja, 300, 100);  
+  drawSprites();
 }
